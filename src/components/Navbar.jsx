3698ca4013fa4957616ed71f960ec821ed43a528
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-01-27 00:02:10 +0530

    add some css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,23 @@ import './Navbar.css'
 function Navbar() {
   return (
     <>
-   
-    <nav className='main-conatiner'>
-      <div className='nav-logo'>
-      <img src="/images/brand_logo.png" alt='#'/>
-      </div>
-      <div className='info'>
-        <a href="">Menu</a>
-        <a href="">Location</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
-      </div>
-      <div className='login'>
-       <button>Login</button>
-      </div>
-    </nav>
+
+      <nav className='main-conatiner'>
+        <div className='nav-logo'>
+          <img src="/images/brand_logo.png" alt='#' />
+        </div>
+        <div className='info'>
+          <a href="">Menu</a>
+          <a href="">Location</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </div>
+        <div className='login'>
+          <button>Login</button>
+        </div>
+      </nav>
     </>
-  
+
   )
 }
 
